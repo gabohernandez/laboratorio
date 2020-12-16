f8ca2a7204550adf36ed4640dbcd8113d7d7b74e
tomascappelli89 <tomascappelli89@gmail.com>
2020-12-16 03:06:57 -0300

    van archivos multimedia

---
+++ app/src/main/java/com/laboratorio/myapplication/CartFragment.java
@@ -0,0 +1,77 @@
+package com.laboratorio.myapplication;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.laboratorio.myapplication.MyProductRecyclerViewAdapter;
+import com.laboratorio.myapplication.R;
+import com.laboratorio.myapplication.model.Product;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class CartFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+    public Map<Long,Integer> products;
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static CartFragment newInstance(int columnCount) {
+        CartFragment fragment = new CartFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.product_item_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                LinearLayoutManager layout = new LinearLayoutManager(context);
+                recyclerView.setLayoutManager(layout);
+                recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), layout.getOrientation()));
+            } else {
+                GridLayoutManager layout = new GridLayoutManager(context, mColumnCount);
+                recyclerView.setLayoutManager(layout);
+                recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), layout.getOrientation()));
+            }
+
+            //recyclerView.setAdapter(new MyProductRecyclerViewAdapter(products));
+        }
+        return view;
+    }
+
+}
